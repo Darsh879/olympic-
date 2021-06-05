3130d32a9bbd342f7f263a592ce2d56287248452
Darsh879 <79584169+Darsh879@users.noreply.github.com>
2021-06-05 16:57:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+canvas =
+document.getElementById("canvas");
+ctx= canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 2;
+ctx.rect(150, 143, 430, 200);
+ctx.strokeStyle();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 1;
+ctx.arc(120, 100, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 1;
+ctx.arc(125, 100, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 1;
+ctx.arc(130, 100, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 1;
+ctx.arc(123, 70, 0, 2*Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.strokeStyle = black;
+ctx.lineWidth = 1;
+ctx.arc(127, 70, 0, 2*Math.PI);
+ctx.stroke();
